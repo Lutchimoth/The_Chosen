e6e819c2b803267a1f080ec760d5168e18710fa7
Lucas Lucier <lucasbr.gto@gmail.com>
2024-04-01 20:10:04 -0300

    commit inicial

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+// -------packages-------
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+
+// ------functions-------
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({outputStyle:'compressed'}))
+    .pipe(gulp.dest('./dist/css'));
+}
+
+function images(){
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+// ------tasks------
+exports.default = gulp.parallel(styles, images);
+exports.watch = function(){
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
